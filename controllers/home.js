efd49c0dc5e3a54fd753e2cdbf37da85be66b722
Caio Incau <caioincau@gmail.com>
2015-03-01 01:41:08 -0300

    sort e estilo

--- controllers/home.js
+++ controllers/home.js
@@ -2,14 +2,17 @@ module.exports = function(app) {
   var Post = app.models.post;
   var HomeController = {
   		index: function(req, res) {
-  			var fluffy = new Post({title: "First Post",private: false, text: "firstPostText"});
-  			fluffy.save(function (err, fluffy) {
-			 if (err) return console.error(err);
-			});
+  	// 		var fluffy = new Post({title: "First Post",private: false, text: "firstPostText"});
+  	// 		fluffy.save(function (err, fluffy) {
+			//  if (err) return console.error(err);
+			// });
       		res.render('home/index');
 		},
 		blog: function(req, res) {
-			var posts= Post.find(function ( err, posts, count ){
+			var query = Post.find();
+			query.sort( {createdAt:-1})
+
+			query.exec(function (err,posts) {
 				params = {posts:posts};
 				res.render('home/posts',params);
 		    });
